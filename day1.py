bd52b172b743fae62bc057e3481e8009b597df6c
juee-exe <fetalynxjue@gmail.com>
2025-05-26 17:19:51 +0700

    Basic_Python_day01
    Practice set 01 date 25/05/2025

---
+++ day1.py
@@ -0,0 +1,30 @@
+# ตัวแปรและการแสดงผล
+
+# Practice No.1: greeting
+name = "Jay"
+print("Hello, my name is", name)
+
+# Practice No.2: easy calculator
+a = 5
+b = 10
+print("The sum of a and b is", a + b)
+
+# Practice No.3: my age
+age = 24
+print("I am", age, "year old.")
+
+# Practice No.4: basic math
+x = 7
+y = 3
+print("Sum:", x + y)
+print("Difference:", x - y)
+print("Product:", x * y)
+print("Quotient:", x / y)
+
+#Practice No.5: what's your hobby?
+name = "Jay"
+age = 25
+hobby = "building Gundam models"
+print(f"My name is {name}.")
+print(f"I am {age} year old.")
+print(f"I like {hobby}.")
